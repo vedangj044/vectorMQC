12c96ffc51c7cc994f181a57f8408a5df01f35f0
vedangj044 <vedangj044@gmail.com>
2021-09-22 23:12:07 +0530

    Basic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
 extern crate qp2p;
-use qp2p::Endpoints;
+extern crate anyhow;
 
-pub fn connect(queue_name: String) {
+use qp2p::{Config, ConnId, Endpoint};
+use anyhow::Result;
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    time::Duration,
+};
 
+pub fn connect(queue_name: String) -> Result<()> {
 
+    let (node, _incoming_conns, mut _incoming_messages, _disconnections, _contact) =
+    Endpoint::new(
+        SocketAddr::from((Ipv4Addr::LOCALHOST, 0)),
+        &[],
+        Config {
+            idle_timeout: Duration::from_secs(60 * 60).into(), // 1 hour idle timeout.
+            ..Default::default()
+        },
+    )
+    .await?;
+
+    Ok(())
 }
